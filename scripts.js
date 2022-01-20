9749968917a130e16cef5326797db07f904c6c30
Lily <89401279+Leelyok@users.noreply.github.com>
2022-01-20 18:17:42 +0300

    add 1st sections

---
+++ scripts.js
@@ -0,0 +1,44 @@
+// Document . ready
+$(document).ready(function() {
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').fadeToggle(100);
+        $(this).toggleClass('active');
+      });
+    
+      $('.mobile-menu a').click(function(){
+        $('.mobile-menu').fadeOut(100);
+        $(".hamburger-button").removeClass('active');
+      });
+
+
+    //Animate scroll - activate
+
+    AOS.init({
+        duration: 1500   // values from 0 to 3000
+    });
+
+});
+
+//LightGallery PopUp
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
+
+//Typed sentences for us//
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 250,
+    backSpeed: 5,
+    
+    loop: true,
+    loopCount: 1,
+    
+    showCursor: false
+  });
