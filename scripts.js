3acd88a01d8c43ae66b3ac270aa85b2cf9ce178c
Lily <89401279+Leelyok@users.noreply.github.com>
2022-02-17 11:40:22 +0300

    section testimonials

--- scripts.js
+++ scripts.js
@@ -39,64 +39,44 @@ $(".burger-menu").click(function(){
 
 });
 
-//LightGallery PopUp
-
-lightGallery(document.getElementById('lightgallery'), {
-    speed: 500,
-    download: false
+$('.slider-container').slick({
+  // Setting name: setting-value
 });
 
-lightGallery(document.getElementById('lightgallery-2'), {
-    speed: 500,
-    download: false
+
+$('.testimon-slider-container').slick({
+  autoplay: true,        // Do we want it to autoplay? true or false
+  autoplaySpeed: 3000,   // How long between each slide when autoplaying
+  speed: 500,            // How fast is the transition 
+  arrows: false,          // Do you want to show arrows to trigger each slide
+  accessibility: true,   // Enables tabbing and arrow key navigation 
+  dots: true,            // Enables the dots below to show how many slides
+  fade: false,           // Changes the animate from slide to fade if true
+  infinite: true,       // When true, means that it will scroll in a circle
+  pauseOnHover: true,   // When true means the autoplay pauses when hovering
+  pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
 });
 
-//Typed sentences for us//
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 250,
-    backSpeed: 5,
-    
-    loop: true,
-    loopCount: 1,
-    
-    showCursor: false
-  });
-
-
-  $('.testimon-slider-container').slick({
-    autoplay: true,        // Do we want it to autoplay? true or false
-    autoplaySpeed: 3000,   // How long between each slide when autoplaying
-    speed: 500,            // How fast is the transition 
-    arrows: false,          // Do you want to show arrows to trigger each slide
-    accessibility: true,   // Enables tabbing and arrow key navigation 
-    dots: true,            // Enables the dots below to show how many slides
-    fade: false,           // Changes the animate from slide to fade if true
-    infinite: true,       // When true, means that it will scroll in a circle
-    pauseOnHover: true,   // When true means the autoplay pauses when hovering
-    pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
-  });
-  
-  $(".testimonial-slider-container").slick({
-      autoplay: true,
-      autoplaySpeed: 2e3,
-      speed: 500,
-      arrows: true,
-      accessibility: true,
-      dots: true,
-      fade: false,
-      infinite: false,
-      pauseOnHover: false,
-      pauseOnDotsHover: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-          }
-      }]
-  });
+$(".testimonial-slider-container").slick({
+    autoplay: true,
+    autoplaySpeed: 2e3,
+    speed: 500,
+    arrows: false,
+    accessibility: true,
+    dots: true,
+    fade: false,
+    infinite: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+        }
+    }]
+});
